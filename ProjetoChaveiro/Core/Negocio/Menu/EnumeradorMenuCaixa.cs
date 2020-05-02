8dc8be2b61b017da4d8b8bb6383a7f0e17374607
loldigow Ferreira <loldigow@hotmail.com>
2020-05-02 14:56:03 -0300

    Intefaces - Adiciona itens de menu

---
+++ ProjetoChaveiro/Core/Negocio/Menu/EnumeradorMenuCaixa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Negocio.Enumeradores
+{
+    public class EnumeradorMenuCaixa : EnumeradorSeguro<string>
+    {
+        private static string NomeFuncao = "Financeiro";
+        public static EnumeradorMenuCaixa Caixa = new EnumeradorMenuCaixa("Caixa", "frmCaixa");
+        public static EnumeradorMenuCaixa AberturaCaixa = new EnumeradorMenuCaixa("Abertura Caixa", "frmAberturaDoCaixa");
+        public static EnumeradorMenuCaixa EmissaoCaixa = new EnumeradorMenuCaixa("Relatorios Caixa", "frmRelatorioDoCaixa");
+
+
+        public EnumeradorMenuCaixa(string codigo, string descricao) : base(codigo, descricao) { }
+    }
+}
---
+++ ProjetoChaveiro/Core/Negocio/Menu/EnumeradorMenuConfiguracoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Negocio.Enumeradores
+{
+    public class EnumeradorMenuConfiguracoes : EnumeradorSeguro<string>
+    {
+        private static string NomeFuncao = "Configurações";
+        public static EnumeradorMenuConfiguracoes Sistema = new EnumeradorMenuConfiguracoes("Configurações do Sistema", "frmConfiguracoes");
+        public static EnumeradorMenuConfiguracoes CadastroOperadores = new EnumeradorMenuConfiguracoes("Cadastro operadores", "frmCadastroOperadores");
+
+        public EnumeradorMenuConfiguracoes(string codigo, string descricao) : base(codigo, descricao) { }
+    }
+}
